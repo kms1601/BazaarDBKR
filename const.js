f3edda26bd8c887d0f575702351e68fa3673b256
kms1601 <kms_1601@naver.com>
2025-10-27 01:43:12 +0900

    const

--- const.js
+++ const.js
@@ -1,12 +1,15 @@
 const PROCESSED_ATTR = "bazaardbkr-processed";
-const CARD = "._aq";
-const CARD_TITLE = "._au";
-const SKILL_CARD_IMAGE = "._ah._ai";
-const MONSTER_CARD_COUNT = "._i";
-const TOOLTIPS = "._bb";
-const EVENT_CARD_IMAGE = "._ah._ak";
-const EVENT_STEP_IMAGE = "._ah._aj";
 
-const DETAIL = "._aK";
-const CARD_DETAIL = "._aa";
+const CARD = "._aq"; // after █ _xx element
+const CARD_TITLE = "._au"; // h3 element
+
+const IMAGE = "._ah"
+const SKILL_CARD_IMAGE = IMAGE + "._ai";
+const EVENT_CARD_IMAGE = IMAGE + "._ak";
+const EVENT_STEP_IMAGE = IMAGE + "._aj";
+
+const MONSTER_CARD_COUNT = "._i"; // tooltip
+
+const DETAIL = "._aK"; // after first element
+const CARD_DETAIL = "._aa"; // first element
 
